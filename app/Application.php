21520ac1fae209c3b66f4773d09c6b70709f88a3
Zander Baldwin <zander.baldwin@basekit.com>
2015-11-11 18:11:04 +0000

    Add Microkernel Symfony Application

---
+++ app/Application.php
@@ -0,0 +1,29 @@
+<?php
+use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
+use Symfony\Component\Config\Loader\LoaderInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\HttpKernel\Kernel;
+use Symfony\Component\Routing\RouteCollectionBuilder;
+
+class Application extends Kernel
+{
+    use MicroKernelTrait;
+
+    public function registerBundles()
+    {
+        return [
+            new Symfony\Bundle\FrameworkBundle\FrameworkBundle,
+            new AppBundle\AppBundle,
+        ];
+    }
+
+    protected function configureContainer(ContainerBuilder $container, LoaderInterface $loader)
+    {
+        $container->register('app.controller.default', 'AppBundle\Controller\DefaultController');
+    }
+
+    protected function configureRoutes(RouteCollectionBuilder $routes)
+    {
+        $routes->add('/', 'app.controller.default:indexAction');
+    }
+}
